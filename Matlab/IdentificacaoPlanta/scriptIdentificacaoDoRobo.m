6ea31455fd617b6497809952204c87cc0e031e2b
jamerico <jean.tome@hotmail.com>
2018-03-03 01:02:18 -0300

    ajustes

--- Matlab/IdentificacaoPlanta/scriptIdentificacaoDoRobo.m
+++ Matlab/IdentificacaoPlanta/scriptIdentificacaoDoRobo.m
@@ -1,22 +1,24 @@
+% load matlab.mat
+
 % correcao com ganho atuador
 gainAtuador = 5/255*0.05/0.0475*2;
-figure
-plot(t,ur,t,uPIDr*gainAtuador)
-
-
-plot(t,ur+0*0.2*sin(2*pi*20*t),t,(theta-90)/30)
+% figure
+% plot(t,ur,t,uPIDr*gainAtuador)
+% 
+% 
+% plot(t,ur+0*0.2*sin(2*pi*20*t),t,(theta-90)/30)
 
 % identification rules
 
 K = 0.8; %% Robot gain
 Kp = 55; %% proportional gain
-sim('identificacaoPlanta.slx')
+% sim('identificacaoPlanta.slx')
 
 a = figure(1);
 % ang
-plot(angSimu.signals.values);
+plot(angSimu.signals.values*180/pi);
 hold on
-plot(angRobot);
+plot(angRobot*180/pi);
 legend('ang simu','ang robot');
 
 intervalAngSimu = angSimu.signals.values(1:100);
@@ -30,5 +32,5 @@ plot(diffTensaoRobot);
 legend('diff tensao simu','diff tensao robot');
 
 
-intervalUSimu = diffTensaoSimu.signals.values(1:100);
-intervalURobot = angdiffTensaoRobot(1:30);
+% intervalUSimu = diffTensaoSimu.signals.values(1:100);
+% intervalURobot = angdiffTensaoRobot(1:30);
